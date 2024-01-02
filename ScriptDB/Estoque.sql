c2d4da2b5feeb455bda9fb6f6818641e46ebf4d6
Fernando Barros <fernandobsak@gmail.com>
2024-01-02 14:03:30 -0300

    adicionando pasta do script do banco

---
+++ ScriptDB/Estoque.sql
@@ -0,0 +1,49 @@
+create database ESTOQUE
+
+use ESTOQUE
+
+CREATE TABLE Usuario (
+	ID int primary key identity, 
+    NomeUsuario VARCHAR(50) unique,
+	Senha varchar(50),
+    NomeCompleto VARCHAR(100),
+    DataNascimento DATE,
+    CPF VARCHAR(14), 
+    Email VARCHAR(100),    
+);
+
+
+CREATE TABLE Estoque (
+    ID INT PRIMARY KEY identity,
+    NomeProduto VARCHAR(100),
+	Fabricante varchar(50),
+    Quantidade INT,
+    PrecoUnitario DECIMAL(10, 2),
+);
+
+select * from Estoque
+select * from Usuario
+
+CREATE TABLE LogEntradaSaida (
+    ID INT PRIMARY KEY identity,
+    ProdutoID INT,
+    Quantidade INT,
+    TipoMovimento varchar(10),
+    DataMovimento DATETIME,
+    FOREIGN KEY (ProdutoID) REFERENCES Estoque(ID)
+);
+
+select * from LogEntradaSaida
+
+create login adm with password='senha1234'
+create user Fernando from login adm;
+
+INSERT INTO CadastroUsuario (Usuario, Senha, NomeCompleto, DataNascimento, CPF, Email)
+VALUES
+('Fernandobsa', '123456', 'Fernando Barros de sa', '1994-12-08', '104.498.264-06', 'fernandobarrosdesak@gmail.com')
+
+--Scaffold-DbContext "Data Source=localhost; initial Catalog=ESTOQUE;User ID=Fernando;password=senha1234;language=Portuguese;Trusted_Connection=True;TrustServerCertificate=True;" Microsoft.EntityFrameworkCore.SqlServer -o "Models"
+
+
+
+
